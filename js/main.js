63867ac4fa46997ca6b3ebb6106405e72eb343f6
Jessica Wu <apf059@hotmail.com>
2020-05-21 23:12:16 +0800

    Add scroll spy

--- js/main.js
+++ js/main.js
@@ -39,3 +39,17 @@ $(document).ready(function() {
     }
   });
 });
+
+$(window).scroll(function() {
+    var currentTop = $(window).scrollTop();
+    var elems = $('.section');
+    elems.each(function(index){
+      var elemTop 	= $(this).offset().top;
+      var elemBottom 	= elemTop + $(this).height();
+      if(currentTop >= elemTop-100 && currentTop <= elemBottom){
+        var id 		= $(this).attr('id');
+        var navElem = $('a[href="#' + id+ '"]');
+    navElem.parent().addClass('active').siblings().removeClass( 'active' );
+      }
+    })
+});
