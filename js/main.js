379885fba7db987c0435f935ef5d9cb348cf09eb
Jessica Wu <apf059@hotmail.com>
2020-05-22 20:25:32 +0800

    Add nav on mobile device

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
 // cover btn scroll
 $(document).ready(function(){
-    $(function(){ $('#btnScroll').click(function(){ 
-        $('html,body').animate({scrollTop:$('#contentWrap').offset().top}, 700);});  
+    $(function(){ 
+      $('#btnScroll').click(function(){ 
+        $('html,body').animate({
+          scrollTop:$('#contentWrap').offset().top
+        }, 700);
+      });
+      $('#btn-about').click(function(){ 
+        $('html,body').animate({
+          scrollTop:$('#about').offset().top
+        }, 700);
+      });
+      $('#btn-projects').click(function(){ 
+        $('html,body').animate({
+          scrollTop:$('#projects').offset().top
+        }, 700);
+      });
+      $('#btn-books').click(function(){ 
+        $('html,body').animate({
+          scrollTop:$('#books').offset().top
+        }, 700);
+      });
+
+      // $('a[href*=#]:not([href=#])').click(function(){
+        // var target = $(this.hash);
+        // $('html,body').stop().animate({
+          // scrollTop:target.offset().top
+        // }, 400);
+        // return false;
+      // })
     }); 
 }); 
+
 
 // animation
 new WOW().init();
@@ -48,8 +76,36 @@ $(window).scroll(function() {
       var elemBottom 	= elemTop + $(this).height();
       if(currentTop >= elemTop-100 && currentTop <= elemBottom){
         var id 		= $(this).attr('id');
-        var navElem = $('a[href="#' + id+ '"]');
-    navElem.parent().addClass('active').siblings().removeClass( 'active' );
+        var navElem = $('li[id="btn-' + id+ '"]');
+        // var navElem = $('a[href="#' + id+ '"]');
+    navElem.addClass('active').siblings().removeClass( 'active' );
+    // navElem.parent().addClass('active').siblings().removeClass( 'active' );
       }
     })
 });
+
+// open mobile menu
+$(document).ready(function(){
+  $('.open-menu').click(function(){
+    $('.menu-mobile-hidden').slideToggle();
+  });
+});
+
+
+$(window).ready(function(){
+  var pageWidth = $(window).width();
+  if(pageWidth < 1150) {
+    $('.menu-mobile-hidden li').click(function(){
+      $('.menu-mobile-hidden').slideToggle('100');
+    });
+  }
+});
+
+// $(document).ready(function(){
+//   $('.stickyNav button').click(function(){
+//     $(this).next().toggleClass('menu-mobile-hidden');
+//   });
+//   $('.menu-mobile-hidden li').click(function(){
+//     $(this).parent().toggleClass('menu-mobile-hidden');
+//   })
+// })
