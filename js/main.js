acbc18e633d1a37b1b6fad7430922dcffd85b35d
apf059 <54426944+apf059@users.noreply.github.com>
2024-09-08 21:32:32 +0800

    Add profile modal

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ new WOW().init();
 
 
 $(function(){ 
+  $(".profile-modal").load("/profile-modal.html"); 
   $(".footer").load("/footer.html"); 
   $(".navigation").load("/navigation.html"); 
   $(".gototop").load("/gototop.html"); 
